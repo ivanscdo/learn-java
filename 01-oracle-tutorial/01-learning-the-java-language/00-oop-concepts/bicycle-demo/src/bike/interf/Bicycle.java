872fa2907aa499873e8d13e90455387db46505f4
Ivan Saucedo <ivan.saucedo@ni.com>
2020-05-10 10:26:32 -0500

    merge new directory with repo
    resumed oracle tutorial
    merging recent changes with existing code

---
+++ 01-oracle-tutorial/01-learning-the-java-language/00-oop-concepts/bicycle-demo/src/bike/interf/Bicycle.java
@@ -0,0 +1,11 @@
+package bike.interf;
+
+public interface Bicycle {
+    void changeCadence(int newValue);
+
+    void changeGear(int newValue);
+
+    void speedUp(int increment);
+
+    void applyBrakes(int decrement);
+}
