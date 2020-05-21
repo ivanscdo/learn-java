985f40adac1f4275f977c4c642421232e1ec87df
Ivan Saucedo <ivan.saucedo@ni.com>
2020-05-20 22:14:14 -0500

    Pascal's Triangle
    completed v1 of project pascals-triangle
    created executable jar
    
    refactored directory structure
    updated README
    created eclipse workspaces for eclipse and maven tutorials

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 # learn-java
 
 ## Tutorial
-Currently following the [official Oracle tutorials.](https://docs.oracle.com/javase/tutorial/index.html)
+Following the [official Oracle tutorials.](https://docs.oracle.com/javase/tutorial/index.html)
 
 ## JDK
-
-Installed Java SE Development Kit 8 (JDK 8), which can be [found here.](https://www.oracle.com/technetwork/java/javase/downloads/index.html)
+Installed [Java SE Development Kit 8 (JDK 8)](https://www.oracle.com/java/technologies/javase-jdk8-downloads.html)
 
 ## IDE
-I installed [netbeans](https://netbeans.apache.org/download/nb100/nb100.html) but, at least for now while I mainly work on practice problems from the turotial and online challenges, I found it much easier to work with VIM (not confident enough to jump into emacs just yet).
+Installed the eclipse package [Eclipse IDE for Java Developers](https://www.eclipse.org/downloads/packages/release/2020-03/r/eclipse-ide-java-developers) 
+
+## Maven
+Installed [maven](https://maven.apache.org/download.cgi)
 
 ## Style Guide
-Following the [Google Java Style Guide.](https://google.github.io/styleguide/javaguide.html)
+Referencing the [Google Java Style Guide.](https://google.github.io/styleguide/javaguide.html)
 
-## Current Contribution
-[Pascal's Triangle](https://github.com/ivanscdo/learn-java/blob/master/00-sandbox/04-pascals-triangle/PascalsTriangle.java).
+## Notable Contributions
+
+
+### Pascal's Tringle - Logic
+[Logic for Pascal's Triangle](sandbox/02-hackerrank/04-pascals-triangle).
 
 This was definitely the most challenging problem yet. It forced me to learn Collections (ArrayLists) and 2D ArrayLists. 
+
+
+### Pascal's Tringle - App
+[Pascal's Triangle App](eclipse/workspace/pascals-triangle)
+
+This is an exercise in building a GUI, along with getting familiar with eclipse and maven. 
---
+++ sandbox/02-hackerrank/04-pascals-triangle/PascalsTriangle.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+
+        // iteration 7
+
+        // number of rows in triangle 
+        int n = Integer.parseInt(args[0]); 
+
+        // sum previous row's elements to build current row
+        int sum = 0; 
+
+        // 2D ArrayList, holds Pascal's Triangle
+        ArrayList<ArrayList<Integer>> pascal = new ArrayList<ArrayList<Integer>>(); 
+
+        // saves previous row to build current row
+        ArrayList<Integer> prevRow = new ArrayList<Integer>();
+
+        // Instantiate all columns of 2D ArrayList
+        for (int i = 0; i < n; i++) {
+            pascal.add(new ArrayList<Integer>());
+        }
+
+        // builds Pascal's Triangle
+        for (int i = 0; i < n; i++) {
+            // builds first 2 rows
+            if (i < 2) {
+                for (int j = 0; j <= i; j++) {
+                    pascal.get(i).add(j, 1);
+                }
+            // builds rows 3 and beyond
+            } else {
+                // adds the first int 1 to current row
+                pascal.get(i).add(0, 1); 
+
+                /**
+                 * adds all middle ints to current row. number of 
+                 * iterations based on numbers in current row minus 2,
+                 * since first and last 1's are added outside loop.
+                 * ex: third row [1, 2, 1] will only iterate once
+                 * to add int 2, while fourth row [1, 3, 3, 1] will
+                 * iterate twice to add int 3 and 3.
+                 **/
+                for (int k = 1; k < prevRow.size(); k++) { 
+                    // clear sum at start of loop
+                    sum = 0;
+                    // sum previous row's elements
+                    sum = prevRow.get(k) + prevRow.get(k-1);
+                    // add previous row's sum to current row in appropriate position
+                    pascal.get(i).add(sum); 
+                }
+
+                // adds the last int 1 to current row
+                pascal.get(i).add(i, 1); 
+            }
+            // clears contents of prevRow, otherwise ALL previous rows would be saved
+            prevRow.clear();
+            // saves current row on pascal to prevRow to be used on next iteration i
+            prevRow.addAll(pascal.get(i));
+        }
+
+        // nicely prints 2D ArrayList, pascal, to console
+        for (int i = 0; i < pascal.size(); i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(pascal.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
